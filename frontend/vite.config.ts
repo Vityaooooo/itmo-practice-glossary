15595453bf25d37a909ee65555a2cd9d631419d6
Ilyasov Viktor <ilasovviktor2666@gmail.com>
2025-01-27 03:43:12 +0300

    feat: edit for github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/itmo-practice-glossary/' // Add this line if and rename it to your repository name if you run your app on GitHub Pages
 })
